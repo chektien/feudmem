02e1c7fc0677de3368ca79b9af597077debe5f62
chektien <chektien.tan@singaporetech.edu.sg>
2020-10-22 07:13:10 +0800

    updated vimrc

--- App.js
+++ App.js
@@ -10,6 +10,56 @@ import {
   Button
 } from "react-native";
 
+// directly declare and export an App component
+// - whatever is exported will be shown on the screen?
+export default function App() {
+  let count = 1;
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={{ color: "#777", fontSize: 20 }}>What I ate...</Text>
+      </View>
+
+      <ScrollView>
+        <View>
+          <Text> This is a scrollview. </Text>
+          <Image
+            source={require("./assets/icon1.png")}
+            style={{ width: 200, height: 200, marginBottom: 10 }}
+          />
+          <TextInput
+            style={{
+              height: 40,
+              padding: 10,
+              borderColor: "gray",
+              borderWidth: 2,
+            }}
+            defaultValue="Type here"
+          />
+        </View>
+        <Repeater count={count++} />
+      </ScrollView>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 50,
+  },
+});
+
 const Repeater = (props) => {
   const [isHungry, setIsHungry] = useState(true);
   return (
@@ -25,46 +75,4 @@ const Repeater = (props) => {
   );
 }
 
-// directly declare and export an App component
-// - whatever is exported will be shown on the screen?
-export default function App() {
-    let count = 1;
-    return (
-      <ScrollView>
-        <View style={styles.container}>
-          <Text> This is a scrollview. </Text>
-          <Image
-            source={{ uri: "https://reactnative.dev/docs/assets/p_cat2.png" }}
-            style={{ width: 200, height: 200 }}
-          />
-          <TextInput
-            style={{
-              height: 40,
-              padding: 5,
-              borderColor: "gray",
-              borderWidth: 2,
-            }}
-            defaultValue="Type here"
-          />
-        </View>
-        <View style={styles.container}>
-          <Text style={{ color: "#999", fontSize: 18 }}>
-            Hello, javascript react world again.
-          </Text>
-          <StatusBar style="auto" />
-        </View>
-        <Repeater count={count++} />
-        <Repeater count={count++} />
-      </ScrollView>
-    );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    padding: 3,
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
