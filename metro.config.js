15255172fad476687db010780455f8a638700424
chektien <chek@gamesstudio.org>
2022-06-05 16:04:02 +0800

    actual fix for the ios firebase bug done, but needed to drop the version to 9.6.11

--- metro.config.js
+++ metro.config.js
@@ -4,3 +4,4 @@ const { getDefaultConfig } = require("expo/metro-config");
 module.exports = getDefaultConfig(__dirname);
 
 module.exports.resolver.assetExts.push("cjs");
+module.exports.resolver.sourceExts.push("cjs");
